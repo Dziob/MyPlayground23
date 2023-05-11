cd6454703b162055c08e2a583d309ba1a21e4487
mtoma <mtomaszewska2@gmail.com>
2023-05-11 15:23:33 +0200

    Sweater qty and size

--- src/main/java/pages/SweaterPage.java
+++ src/main/java/pages/SweaterPage.java
@@ -1,9 +1,15 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SweaterPage {
     public SweaterPage (WebDriver driver) {
@@ -19,7 +25,28 @@ public class SweaterPage {
     @FindBy(id = "quantity_wanted")
     WebElement sweaterQty;
 
+    @FindBy(className = "add")
     WebElement addToCart;
+
+    public String sweaterDiscountCheck(){
+        return sweaterDiscount.getText();
+    }
+
+    public void chooseSweaterSize(){
+        Select size = new Select(sweaterSize);
+        size.selectByVisibleText("M");
+    }
+
+    public void chooseQty(){
+        sweaterQty.sendKeys(String.valueOf(5));
+    }
+
+    public void AddSweaterToCart(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.textToBe((By) sweaterQty, "5"));
+        addToCart.click();
+    }
+
 
 
 
--- src/main/java/steps/ChangingQuantity.java
+++ src/main/java/steps/ChangingQuantity.java
@@ -1,7 +1,34 @@
 package steps;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import pages.ProductPage;
+import pages.SweaterPage;
 
 public class ChangingQuantity {
+
     public static WebDriver driver;
+
+    public String url = "https://mystore-testlab.coderslab.pl/index.php?id_product=2&id_product_attribute=9&rewrite=brown-bear-printed-sweater&controller=product#/1-size-s";
+
+    @Given("The user is on the product page - 'Hummingbird Printed Sweater'")
+    public void setUp() {
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+    }
+
+    @And("Checking for a discount and chooses the 'M' size")
+    public void productDetails(){
+        SweaterPage sweaterPage = new SweaterPage(driver);
+        Assert.assertEquals(sweaterPage.sweaterDiscountCheck(), "SAVE 20%");
+        sweaterPage.chooseSweaterSize();
+
+    }
+
 }
